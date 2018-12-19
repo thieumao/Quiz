9a30a922eb8c922b453298938fe4557fbc1462a8
Nguyen Van Thieu <nguyen.van.thieu@linecorp.com>
2018-12-19 14:10:14 +0700

    Basic Tabbar

---
+++ Quiz/Screens/Profile/ProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileVC.swift
+//  Quiz
+//
+//  Created by Nguyen Van Thieu on 12/19/18.
+//  Copyright © 2018 Nguyen Van Thieu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
